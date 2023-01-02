cd0745fcf3be136acf9ba920b644b6093624c1a0
tobby8629 <sarhmullah15@gmail.com>
2023-01-02 02:18:18 -0800

    add functionality to home and about header

---
+++ script.js
@@ -0,0 +1,9 @@
+const bars = document.querySelector('.icon');
+const tray = document.querySelector('#tray');
+
+
+bars.addEventListener('click', ()=>{
+    tray.classList.toggle('show');
+    bars.classList.toggle('how');
+
+})
