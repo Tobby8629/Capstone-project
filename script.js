5f963b0f42940869ee5405c0513f5f320f95fc5d
tobby8629 <sarhmullah15@gmail.com>
2023-01-04 19:24:36 -0800

    rectifying deploy errors

--- script.js
+++
@@ -1,9 +0,0 @@
-const bars = document.querySelector('.icon');
-const tray = document.querySelector('#tray');
-
-
-bars.addEventListener('click', ()=>{
-    tray.classList.toggle('show');
-    bars.classList.toggle('how');
-
-})
